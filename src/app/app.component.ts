1a6a5bbacb524aa93474b24c01015f3f77ddb50d
Rodrigo <rodrigdsoto@gmail.com>
2022-05-31 13:09:29 -0300

    Signed-off-by: Rodrigo <rodrigdsoto@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,38 @@ import { inject } from '@angular/core/testing';
 export class AppComponent implements OnInit,AfterContentInit {
   
   title = 'sidebar5';
+  colorhover="";
   
   
   ngOnInit(): void {
     this.controlMenu();
+      
   }
   ngAfterContentInit(): void {
     //this.controlMenu();
+  }
+  
+  setcolor(color:string){ 
+    const a=document.querySelectorAll(".item-menu .item-menu-label");
+    for(let i=0;i<a.length;i++){ 
+      a[i].classList.remove("HD","HG","HPI","HBL");
+      switch(color){
+        case "#d81b60":
+          a[i].classList.add("HPI");        
+          break;
+        case "#388e3c":       
+          a[i].classList.add("HG");          
+          break;
+        case "#283046":
+          a[i].classList.add("HD");  
+          break;
+        case "#1e88e5":
+          a[i].classList.add("HBL");  
+          break;
+          
+      }
+    }
+   
   }
   
   controlMenu(){
@@ -42,13 +67,14 @@ export class AppComponent implements OnInit,AfterContentInit {
           for(let j=0;j<li.length;j++){
             li[j].children[0].classList.remove("active");
           }
+          li[i].children[0].children[2].classList.toggle("open");
           li[i].children[2].classList.toggle("collapse")
         }
       });
     }   
     
     // TODO: boton para cambiar aspecto claro a obscuro
-    dianoche?.addEventListener("click",()=>{
+      dianoche?.addEventListener("click",()=>{
       document.getElementsByTagName("body")[0].classList.toggle("dark");
       document.getElementById("sidebar")?.classList.toggle("dark-mode");
       
